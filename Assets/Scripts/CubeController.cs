d4683707cdbc01eb150d03a37b61bbdcf783f865
Milin00 <wtnbryt1122@gmail.com>
2025-08-12 16:04:31 +0900

    移動

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+
+public class CubeController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private float jumpForce = 8.0f;
+    private Vector3 move;
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3(move.x, 0, move.z);
+        transform.Translate(moveDirection, Space.World);
+    }
+
+    private void OnMove(InputValue val)
+    {
+        Vector2 input = val.Get <Vector2 >();
+        move = new Vector3(input.x,0,input.y)* moveSpeed * Time.deltaTime;  
+    }
+
+    private void OnJump(InputValue val)
+    {
+        if (val.isPressed)
+        {
+            rb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+        }
+    }
+}
